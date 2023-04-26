d3ea495a1807165db0f4377ecb1d869ad4039d5b
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-04-26 20:05:00 -0300

    Creating useSnack hook.

--- src/pages/Main/Desserts/index.tsx
+++ src/pages/Main/Desserts/index.tsx
@@ -1,13 +1,11 @@
-import { useContext } from 'react';
-
 import { Head } from '../../../components/Head';
 import SnackTitle from '../../../components/SnackTitle';
 import Snacks from '../../../components/Snacks';
 
-import { SnackContext } from '../../../contexts/SnackContext';
+import { useSnack } from '../../../hooks/useSnack';
 
 export default function Desserts() {
-    const { desserts } = useContext(SnackContext);
+    const { desserts } = useSnack();
 
     return (
         <>
